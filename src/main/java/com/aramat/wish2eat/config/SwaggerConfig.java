42d0bd7b1a0e20babf9cb6faf54f730a6fe5f6b0
Jonas Ferreira <jonas.edu@hotmail.com>
2021-05-13 23:42:43 -0300

    Configurações e Cors

--- src/main/java/com/aramat/wish2eat/config/SwaggerConfig.java
+++ src/main/java/com/aramat/wish2eat/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.aramat.wish2eat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -12,7 +13,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30).select()
-                .apis(RequestHandlerSelectors.basePackage("com.aramat.wish2eat.resources"))
-                .paths(PathSelectors.any()).build();
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+                .build();
     }
 }
